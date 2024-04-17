7fd6fdb91687d437aaf5f2c0a5eace2cf8e6c630
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-04-16 21:51:14 -0300

    update

--- bmp180.py
+++ bmp180.py
@@ -2,12 +2,15 @@ from machine import I2C, Pin
 from bmp180 import BMP180
 import utime
 
-i2c = I2C(id=0, scl=Pin(1), sda=Pin(0), freq=100000)
 
 # Inicializa a comunicação I2C com o ID 0, definindo os pinos SCL e SDA e a frequência de comunicação (100 kHz)
+i2c = I2C(id=0, scl=Pin(1), sda=Pin(0), freq=100000)
+
 bmp180 = BMP180(i2c)
+# Define a configuração de oversampling para o sensor (maior valor significa maior precisão, mas mais tempo de leitura)
 bmp180.oversample_sett = 2
 bmp180.baseline = 101325
+
 while True:
     p = bmp180.pressure
     print("Pressão:", p, "Pa")
