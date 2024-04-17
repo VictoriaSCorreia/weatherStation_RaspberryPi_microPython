b982e48dc2785f5095a348d92bc68469e4e36def
VictoriaSCorreia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-04-16 21:50:16 -0300

    update

--- bmp180.py
+++ bmp180.py
@@ -4,6 +4,7 @@ import utime
 
 i2c = I2C(id=0, scl=Pin(1), sda=Pin(0), freq=100000)
 
+# Inicializa a comunicação I2C com o ID 0, definindo os pinos SCL e SDA e a frequência de comunicação (100 kHz)
 bmp180 = BMP180(i2c)
 bmp180.oversample_sett = 2
 bmp180.baseline = 101325
