c362f3048c1bf27e7acc06ade7d0950ae94c0c07
Victoria Correia <104169748+VictoriaSCorreia@users.noreply.github.com>
2024-06-10 19:08:06 -0300

    Update bmp180.py

--- bmp180.py
+++ bmp180.py
@@ -1,18 +1,110 @@
+'''
+bmp180 is a micropython module for the Bosch BMP180 sensor. It measures
+temperature as well as pressure, with a high enough resolution to calculate
+altitude.
+Breakoutboard: http://www.adafruit.com/products/1603  
+data-sheet: http://ae-bst.resource.bosch.com/media/products/dokumente/
+bmp180/BST-BMP180-DS000-09.pdf
+
+The MIT License (MIT)
+Copyright (c) 2014 Sebastian Plamauer, oeplse@gmail.com
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+from ustruct import unpack as unp
 from machine import I2C, Pin
-from bmp180 import BMP180
-import utime
+import math
+import time
 
+# BMP180 class
+class BMP180():
+    '''
+    Module for the BMP180 pressure sensor.
+    '''
 
-# Inicializa a comunicação I2C com o ID 0, definindo os pinos SCL e SDA e a frequência de comunicação (100 kHz)
-i2c = I2C(id=0, scl=Pin(1), sda=Pin(0), freq=100000)
+    _bmp_addr = 119             # adress of BMP180 is hardcoded on the sensor
 
-bmp180 = BMP180(i2c)
-# Define a configuração de oversampling para o sensor (maior valor significa maior precisão, mas mais tempo de leitura)
-bmp180.oversample_sett = 2
-# Define o valor de referência da pressão ao nível do mar em Pascal (101325 Pa)
-bmp180.baseline = 101325
+    # init
+    def __init__(self, i2c_bus):
 
-while True:
-    p = bmp180.pressure
-    print("Pressão:", p, "Pa")
-    utime.sleep(5)
+        # create i2c obect
+        _bmp_addr = self._bmp_addr
+        self._bmp_i2c = i2c_bus
+        self._bmp_i2c.start()
+        self.chip_id = self._bmp_i2c.readfrom_mem(_bmp_addr, 0xD0, 2)
+        # read calibration data from EEPROM
+        self._AC1 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xAA, 2))[0]
+        self._AC2 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xAC, 2))[0]
+        self._AC3 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xAE, 2))[0]
+        self._AC4 = unp('>H', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xB0, 2))[0]
+        self._AC5 = unp('>H', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xB2, 2))[0]
+        self._AC6 = unp('>H', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xB4, 2))[0]
+        self._B1 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xB6, 2))[0]
+        self._B2 = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xB8, 2))[0]
+        self._MB = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xBA, 2))[0]
+        self._MC = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xBC, 2))[0]
+        self._MD = unp('>h', self._bmp_i2c.readfrom_mem(_bmp_addr, 0xBE, 2))[0]
+
+        # settings to be adjusted by user
+        self.oversample_setting = 3
+        self.baseline = 101325.0
+
+        # output raw
+        self.UT_raw = None
+        self.B5_raw = None
+        self.MSB_raw = None
+        self.LSB_raw = None
+        self.XLSB_raw = None
+        self.gauge = self.makegauge() # Generator instance
+        for _ in range(128):
+            next(self.gauge)
+            time.sleep_ms(1)
+
+    def compvaldump(self):
+        '''
+        Returns a list of all compensation values
+        '''
+        return [self._AC1, self._AC2, self._AC3, self._AC4, self._AC5, self._AC6, 
+                self._B1, self._B2, self._MB, self._MC, self._MD, self.oversample_setting]
+
+    # gauge raw
+    def makegauge(self):
+        '''
+        Generator refreshing the raw measurments.
+        '''
+        delays = (5, 8, 14, 25)
+        while True:
+            self._bmp_i2c.writeto_mem(self._bmp_addr, 0xF4, bytearray([0x2E]))
+            t_start = time.ticks_ms()
+            while (time.ticks_ms() - t_start) <= 5: # 5mS delay
+                yield None
+            try:
+                self.UT_raw = self._bmp_i2c.readfrom_mem(self._bmp_addr, 0xF6, 2)
+            except:
+                yield None
+            self._bmp_i2c.writeto_mem(self._bmp_addr, 0xF4, bytearray([0x34+(self.oversample_setting << 6)]))
+            t_pressure_ready = delays[self.oversample_setting]
+            t_start = time.ticks_ms()
+            while (time.ticks_ms() - t_start) <= t_pressure_ready:
+                yield None
+            try:
+                self.MSB_raw = self._bmp_i2c.readfrom_mem(self._bmp_addr, 0xF6, 1)
+                self.LSB_raw = self._bmp_i2c.readfrom_mem(self._bmp_addr, 0xF7, 1)
+                self.XLSB_raw = self._bmp_i2c.readfrom_mem(self._bmp_addr, 0xF8, 1)
+            except:
+                yield None
+            yield True
