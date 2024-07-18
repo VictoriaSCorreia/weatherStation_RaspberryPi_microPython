74209d6b734370d64c773f663f897f871e93dd66
VictoriaSCorreia <victoriacorreiavic@gmail.com>
2024-07-18 16:12:48 -0300

    Creating applcd

---
+++ LCD/AppLcd.py
@@ -0,0 +1,19 @@
+from machine import Pin
+from machine import I2C
+import time
+
+from pico_i2c_lcd import I2cLcd
+from lcd_api import LcdApi
+
+import dht
+
+# LCD configuration
+sda=Pin(0)
+scl=Pin(1)
+i2c=I2C(0,sda=sda, scl=scl, freq=400000)
+endereco = 0x27
+linhas = 2
+colunas = 16
+lcd = I2cLcd(i2c,endereco,linhas,colunas)
+
+
